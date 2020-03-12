9b6051d3057858fcfc58aba1ea3076bf16d73f71
junggu <junggu.ji.dev@gmail.com>
2020-03-12 16:52:45 +0900

    type : Refactor
    불필요한 import 삭제

--- src/main/java/com/jgji/givememask/config/SecurityConfig.java
+++ src/main/java/com/jgji/givememask/config/SecurityConfig.java
@@ -3,14 +3,10 @@ package com.jgji.givememask.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
